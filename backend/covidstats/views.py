eb1bc2a774d1f9dd13014d3babb58ee3614bbdc0
Evgeniy Mokich <mokichzhenya@gmail.com>
2024-01-22 11:17:45 +0100

    test

--- backend/covidstats/views.py
+++ backend/covidstats/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import pandas as pd
 import requests
+import tailer
+import io
 
 covid_data_dir = 'data/'
 covid_data_link = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
@@ -14,14 +16,21 @@ target_year = '2023'
 @api_view(['GET'])
 def get_covid_data(request):
     download_data()
-    df = pd.read_csv(data_dir)[columns]
+    with open(data_dir) as f:
+        last_lines = tailer.tail(f, 20)
+    df = pd.read_csv(io.StringIO('\n'.join(last_lines)), header=None)
+    column_indices = list(range(2, 6))
+    df = df[column_indices]
     df.dropna(inplace=True)
+    print(df)
+    # df.dropna(inplace=True)
 
-    return Response("HELLO")
+    # df_up_to_date = df_up_to_date[df_up_to_date['location'] == location]
+    return Response(df.to_numpy())
 
 
 def download_data():
     resp = requests.get(covid_data_link)
     with open(data_dir, '+wb') as f:
         f.write(resp.content)
-    del resp
+    # del resp
